808ea655438caf017b7251150b693303e029f086
RyancoRitter <nakiusagi3jp@outlook.jp>
2025-05-17 11:18:48 +0900

    バージョン表記更新(1.06)

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -101,7 +101,7 @@ namespace MyKittenPaint.Properties {
         }
         
         /// <summary>
-        ///   Ver.1.05 (250517-A) に類似しているローカライズされた文字列を検索します。
+        ///   Ver.1.06 (250517-A) に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string SoftVerStr {
             get {
